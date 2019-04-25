4fa20673edaf7834c30d9373d0d7723cd1bc9b22
Scott Brady <scott@scottbrady91.com>
2019-04-25 12:57:36 +0100

    Quickstart 1 - core installation

---
+++ Core/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TestChallenge()
+        {
+            var result = await HttpContext.AuthenticateAsync("cookie");
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return Challenge("openid-1");
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync("cookie");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Core/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Logging;
+
+namespace Core
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            IdentityModelEventSource.ShowPII = true;
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddMvc();
+
+            services.AddAuthentication("cookie")
+                .AddCookie("cookie");
+
+            services.AddDynamicProviders() // Component setup
+                .AddJsonStore(options => options.Path = "schemes.json") // Basic JSON store for auth schemes
+                .AddOpenIdConnect() // Add OIDC support
+                .AddSaml(); // Add SAML support
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
