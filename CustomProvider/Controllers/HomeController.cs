a8bdc0455a1c9536226678c017b06c60160d6a64
Scott Brady <scott@scottbrady91.com>
2019-07-05 15:10:19 +0100

    Quickstart 3 - custom provider

---
+++ CustomProvider/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomProvider.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TestChallenge()
+        {
+            var result = await HttpContext.AuthenticateAsync("cookie-1");
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return Challenge("cookie-1");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(string returnUrl)
+        {
+            await HttpContext.SignInAsync("cookie-1", new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> {new Claim("sub", "123")})));
+            return Redirect(returnUrl);
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync("cookie-1");
+            return RedirectToAction("Index");
+        }
+    }
+}
